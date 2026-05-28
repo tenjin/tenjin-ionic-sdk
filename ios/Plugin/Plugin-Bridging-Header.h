90619efa6c5e12ae61c3620d51f34e7bb7095a05
Diego Giraldo <diegoagg95@gmail.com>
2026-05-28 13:20:07 -0500

    feat: add Capacitor 8 with SPM support [TENJIN-27741] (#24)
    * feat: add Capacitor 8 with SPM support [TENJIN-27741]
    
    * feat: add Capacitor 8 with SPM support [TENJIN-27741]
    
    * chore: remove CLAUDE.md - update Android SDK

--- ios/Plugin/Plugin-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Plugin-Bridging-Header.h
-//  Plugin
-//
-//  Created by Diego Giraldo on 28/12/22.
-//  Copyright © 2022 Max Lynch. All rights reserved.
-//
-
-#import "TenjinImplementation.h"
